ae5db43f1a8ef22b36955468b0978fbc130f8a02
Roberto Ramos Trujillo <53947217+YoHaylander@users.noreply.github.com>
2023-01-23 11:24:08 -0500

    Se creo el model SalesControl

---
+++ WebAppCore/Model/SalesControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCore.Model
+{
+    public class SalesControl
+    {
+        public int Id { get; set; }
+        public DateTime DateSale { get; set; }
+        public string seller { get; set; }
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public string Customer { get; set; }
+    }
+}
