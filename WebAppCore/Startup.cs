003bbee2957aa2f3b92a732e4b93eaf67b74a9c0
Roberto Ramos Trujillo <53947217+YoHaylander@users.noreply.github.com>
2023-01-23 11:41:18 -0500

    Se configuro JWT

--- WebAppCore/Startup.cs
+++ WebAppCore/Startup.cs
@@ -1,21 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebAppCore.DatabaseContext;
+using System.Text;
 
 namespace WebAppCore
 {
     public class Startup
     {
+        private const string SECRET_KEY = "TQvgjeABMPOwCycOqah5EQu5yyVjpmVGTQvgjeABMPOwCycOqah5EQu5yyVjpmVGTQvgjeABMPOwCycOqah5EQu5yyVjpmVG";
+        public static readonly SymmetricSecurityKey SIGNING_KEY = new
+            SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,8 +27,33 @@ namespace WebAppCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<MyContext>(options => options.UseInMemoryDatabase("SalesControl"));
             services.AddControllers();
+            services.AddApiVersioning(option =>
+            {
+                option.ReportApiVersions = true;
+                option.DefaultApiVersion = new ApiVersion(1, 0);
+                option.AssumeDefaultVersionWhenUnspecified = true;
+            });
+            // Configure the JWT Authentication Service
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            })
+                .AddJwtBearer("JwtBearer", jwtOptions =>
+                {
+                    jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        // The SigningKey is defined in the TokenController class
+                        IssuerSigningKey = SIGNING_KEY,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidIssuer = "https://localhost:44356",
+                        ValidAudience = "https://localhost:44356",
+                        ValidateLifetime = true
+                    };
+                });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAppCore", Version = "v1" });
